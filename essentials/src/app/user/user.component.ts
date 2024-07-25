4f91f4de50c984b146d900e4a5418cf1de98be59
Deepak <43214453+amigos-code@users.noreply.github.com>
2024-07-25 08:56:26 +0530

    Updating UI status with signals

--- essentials/src/app/user/user.component.ts
+++ essentials/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , computed, signal} from '@angular/core';
 import { DUMMY_USERS } from '../../dummy-users';
 
 //This will give random index value from the length od DUMMY_USERS list.
@@ -11,18 +11,28 @@ const randomIndex= Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-   selectedUser=DUMMY_USERS[randomIndex];
+
+
+
+
+
+  //  selectedUser=DUMMY_USERS[randomIndex];
+  //Introduced signals with the initial value
+  selectedUser=signal(DUMMY_USERS[randomIndex]);
+//Computed is used with singal 
+  imagePath= computed(()=>'/assets/users/' + this.selectedUser().avatar )
 
   //  Below getter method for computing the path of image
   //get keyword marked imagePath as Getter.
-  // While using in template file it will used as attribute  (imagePath) not metho
-   get imagePath(){
-    return '/assets/users/' + this.selectedUser.avatar ;
-   }
+  // While using in template file it will used as attribute  (imagePath) not method
+  //  get imagePath(){
+  //   return computed('/assets/users/' + this.selectedUser.avatar );
+  //  }
 
    //Angular change detection works on library called zone.js
    onSelectUser(){
     const randomIndex= Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser=DUMMY_USERS[randomIndex];
+    // this.selectedUser=DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
    }
 }
